96a17852e8f887a9c72195287953651279add5d9
beccaruffalo25 <beccaruffalo25@gmail.com>
2026-02-23 14:49:15 -0600

    Delete scripts.js

--- scripts.js
+++
@@ -1,9 +0,0 @@
-(function(){
-  // nav background on scroll
-  var nav=document.getElementById('nav');
-  if(nav){
-    window.addEventListener('scroll',function(){
-      nav.classList.toggle('on',window.scrollY>50);
-    });
-  }
-})();
